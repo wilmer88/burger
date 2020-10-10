c3d75572ff84cd62fe07c2665a0736c9a6a0da08
wilmerbaby <wilmerbaby@gmail.com>
2020-10-10 14:51:41 -0400

    injected test  value in html.handlbars

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//get rout to render the whats in the index file in main.handlbars file
+//get rout to render the whats in the index file in main.handlbars file also able to inject values when you res.render
+//rendering into index to show on main handlebars file
 app.get("/",(req, res) =>{
-    res.render("index");
+    res.render("index", {name: "Wilmer Rivera Morter"});
 });
 // test rout/get rout to a web browser api page 
 app.get("/api/config",(req, res) => {

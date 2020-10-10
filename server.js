1f328d756b2345281a2f2707e4c4bc1b3be0eb4d
wilmerbaby <wilmerbaby@gmail.com>
2020-10-10 14:08:11 -0400

    added middle ware for both app and handlebars

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const exphbs = require("express-handlebars");
 const mysql = require("mysql");
 const app = express();
 const PORT = process.env.PORT || 8080;
+//app middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//midleware for handelbars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//get rout to render the whats in the index file in main.handlbars file
+app.get("/",(req, res) =>{
+    res.render("index");
+});
+// test rout/get rout to a web browser api page 
 app.get("/api/config",(req, res) => {
     res.json({
         success:true,
